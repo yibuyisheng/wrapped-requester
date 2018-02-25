3b5ee8ed2028bc18f419166c327a243ec073163b
yibuyisheng <yibuyisheng@163.com>
2018-02-25 22:29:43 +0800

    remove types

--- types/Event.d.ts
+++
@@ -1,6 +0,0 @@
-export default class Event {
-    private handlerMap;
-    on(eventName: string, handler: () => void): void;
-    off(eventName: string): void;
-    emit(eventName: string, ...args: any[]): void;
-}
--- types/IOptions.d.ts
+++
@@ -1,3 +0,0 @@
-export default interface IOptions {
-    [key: string]: any;
-}
